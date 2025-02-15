130d1c7aff049990b4e6ccfff8c26f8e2b76e16b
Thomas shikalepo deon <thomasshikalepo@gmail.com>
2025-02-15 15:46:50 +0200

    update the page when delete button is clicked

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach(
             }
         );
 
-        cartSummaryHTML += `<div class="cart-item-container">
+        cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProducts.id}">
                 <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
                 <div class="cart-item-details-grid">
@@ -97,7 +97,10 @@ document.querySelectorAll('.js-delete-link').forEach(
 
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            console.log(cart)
+
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`);
+            container.remove();
         });
     }
 )
